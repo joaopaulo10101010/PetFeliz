48ec39131e9f0179745c10c8907f2c988f527d20
joaopaulo10101010 <joao_paulo_oandrade@hotmail.com>
2024-11-25 15:13:23 -0300

    implantando o carrossel 1/3

---
+++ javascript/java.js
@@ -0,0 +1,13 @@
+const carrossel = document.querySelector('.carrossel');
+const imagens = document.querySelectorAll('.carrossel img');
+const contador = document.getElementById("contador");
+
+let indice = 0;
+
+
+function mostrarProximaImagem() {
+    indice = (indice + 1) % imagens.length; // Alterna para a próxima imagem, reiniciando no final
+    carrossel.style.transform = `translateX(${-indice * contador.width}px)`; // Move o carrossel
+}
+
+setInterval(mostrarProximaImagem, 3000); // Alterna a cada 3 segundos
